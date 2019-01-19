26d105ea954d41ecab0a1be36ca3669565f37772
Piotr <piotr.michalski2000@gmail.com>
2019-01-19 12:13:28 +0100

    Add all files

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/* eslint no-console: 0 */
+
+import './scss/main.scss';
+import './js/scripts.js';
+console.log(`I'm a silly entry point`);
+
---
+++ src/js/scripts.js
@@ -0,0 +1,29 @@
+// Hamburger menu
+const nav = document.querySelector('.nav__list');
+const hamburger = document.querySelector('.hamburger');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerLabel = document.querySelector('.hamburger-label');
+
+hamburger.addEventListener('click', (event) => {
+    event ? event : event = window.event;
+    event.preventDefault();
+    hamburgerMenu.classList.toggle('animate');
+    nav.classList.toggle('nav__list--block');
+
+    if (hamburgerLabel.textContent === "Otwórz menu") {
+        hamburgerLabel.textContent = "Zamknij menu";
+    } else {
+        hamburgerLabel.textContent = "Otwórz menu";
+    }
+
+})
+
+if (window.innerWidth <= 500) {
+
+    const hide = () => {
+        nav.classList.remove('nav__list--block');
+    };
+
+    const links = [ ...document.querySelectorAll('.clicked') ];
+    links.forEach((link) => link.addEventListener('click', hide));
+}
